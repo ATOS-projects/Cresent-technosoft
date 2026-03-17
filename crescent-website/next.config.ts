61007b4df0c892b1a2ffa3e0876e7fd6c701928b
Adarsh-P-Thomson <131739584+Adarsh-P-Thomson@users.noreply.github.com>
2026-03-17 09:26:02 +0530

    pages deploy

--- crescent-website/next.config.ts
+++ crescent-website/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Cresent-technosoft',
+  assetPrefix: '/Cresent-technosoft',
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
