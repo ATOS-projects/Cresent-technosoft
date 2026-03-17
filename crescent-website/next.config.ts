02dd8bf5f152d91a9b3ed4640f49cb3c80e2390d
Adarsh-P-Thomson <131739584+Adarsh-P-Thomson@users.noreply.github.com>
2026-03-17 10:27:39 +0530

    Update next.config.ts

--- crescent-website/next.config.ts
+++ crescent-website/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = '';
+if (isGithubActions) {
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'Cresent-technosoft';
+  repo = `/${repoName}`;
+}
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: repo !== '' ? repo : undefined,
+  assetPrefix: repo !== '' ? repo : undefined,
   images: {
     unoptimized: true,
   }
