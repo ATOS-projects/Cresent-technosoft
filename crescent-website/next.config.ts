c8f33b3f31752287e5fa191108ad9372660cfc14
Adarsh-P-Thomson <131739584+Adarsh-P-Thomson@users.noreply.github.com>
2026-03-17 10:12:34 +0530

    image loading issue

--- crescent-website/next.config.ts
+++ crescent-website/next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  // Apply basePath only when building for production
-  basePath: isProd ? '/Cresent-technosoft' : '',
-  assetPrefix: isProd ? '/Cresent-technosoft' : '',
-  images: {
-    unoptimized: true,
-  }
+  /* config options here */
 };
 
 export default nextConfig;
